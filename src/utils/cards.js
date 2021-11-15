0014768f73fdcf085f254d50e869a7185dd0cc73
Escias <ljimmy98@hotmail.fr>
2021-11-15 18:42:09 +0100

    update 'cards' and 'crud' constructor

--- src/utils/cards.js
+++ src/utils/cards.js
@@ -1,5 +1,5 @@
 class Cards {
-    constructor(id, member, description, tag, checklist, deadline, lastUpdate, timeCreation) {
+    constructor(id, member, description, tag, checklist, deadline, lastUpdate) {
         this._id = id
         this._member = member
         this._description = description
@@ -7,30 +7,18 @@ class Cards {
         this._checklist = checklist
         this._deadline = deadline
         this._lastUpdate = lastUpdate
-        this._timeCreation = timeCreation
+        this._timeCreation = Date.now()
     }
     get(){
         return [this._id, this._member, this._description, this._tag, this._checklist, this._deadline, this._lastUpdate, this._timeCreation]
     }
 
-    setCreate(id, member, description, tag, checklist, deadline, lastUpdate, timeCreation){
-        this._id = id
-        this._member = member
-        this._description = description
-        this._tag = tag
-        this._checklist = checklist
-        this._deadline = deadline
-        this._lastUpdate = lastUpdate
-        this._timeCreation = timeCreation
-    }
-
-    setUpdate(newMember, newDescription, newTag, newChecklist, newDeadline, newLastUpdate, newTimeCreation){
+    setUpdate(newMember, newDescription, newTag, newChecklist, newDeadline){
         this._member = newMember
         this._description = newDescription
         this._tag = newTag
         this._checklist = newChecklist
         this._deadline = newDeadline
-        this._lastUpdate = newLastUpdate
-        this._timeCreation = newTimeCreation
+        this._lastUpdate = Date.now()
     }
 }
--- src/utils/crud.js
+++ src/utils/crud.js
@@ -2,8 +2,15 @@ class Crud{
     constructor(nameList, cards, timeCreation) {
         this._nameList = nameList
         this._cards = cards
-        this._timeCreation = timeCreation
+        this._timeCreation = Date.now()
     }
 
+    get(){
+        return [this._nameList, this._cards, this._timeCreation]
+    }
 
+    setUpdate(newNameList, newCards){
+        this._nameList = newNameList
+        this._cards = newCards
+    }
 }
