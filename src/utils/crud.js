53c54b03c512dcc7f31a28ccbc6a9504cd5ad1c7
Escias <ljimmy98@hotmail.fr>
2021-11-15 18:30:00 +0100

    create constructor 'crud'

---
+++ src/utils/crud.js
@@ -0,0 +1,9 @@
+class Crud{
+    constructor(nameList, cards, timeCreation) {
+        this._nameList = nameList
+        this._cards = cards
+        this._timeCreation = timeCreation
+    }
+
+
+}
